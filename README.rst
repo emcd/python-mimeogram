380c5042a79342dd5f5bc37e9dfa85374b932042
Eric McDonald <emcd@users.noreply.github.com>
2025-01-11 16:16:23 -0800

    Initial population from Copier template.

---
+++ README.rst
@@ -0,0 +1,94 @@
+.. vim: set fileencoding=utf-8:
+.. -*- coding: utf-8 -*-
+.. +--------------------------------------------------------------------------+
+   |                                                                          |
+   | Licensed under the Apache License, Version 2.0 (the "License");          |
+   | you may not use this file except in compliance with the License.         |
+   | You may obtain a copy of the License at                                  |
+   |                                                                          |
+   |     http://www.apache.org/licenses/LICENSE-2.0                           |
+   |                                                                          |
+   | Unless required by applicable law or agreed to in writing, software      |
+   | distributed under the License is distributed on an "AS IS" BASIS,        |
+   | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. |
+   | See the License for the specific language governing permissions and      |
+   | limitations under the License.                                           |
+   |                                                                          |
+   +--------------------------------------------------------------------------+
+
+*******************************************************************************
+                                  python-mimeogram
+*******************************************************************************
+
+.. image:: https://img.shields.io/pypi/v/python-mimeogram
+   :alt: Package Version
+   :target: https://pypi.org/project/python-mimeogram/
+
+.. image:: https://img.shields.io/pypi/status/python-mimeogram
+   :alt: PyPI - Status
+   :target: https://pypi.org/project/python-mimeogram/
+
+.. image:: https://github.com/emcd/python-mimeogram/actions/workflows/tester.yaml/badge.svg?branch=master&event=push
+   :alt: Tests Status
+   :target: https://github.com/emcd/python-mimeogram/actions/workflows/tester.yaml
+
+.. image:: https://emcd.github.io/python-mimeogram/coverage.svg
+   :alt: Code Coverage Percentage
+   :target: https://github.com/emcd/python-mimeogram/actions/workflows/tester.yaml
+
+.. image:: https://img.shields.io/github/license/emcd/python-mimeogram
+   :alt: Project License
+   :target: https://github.com/emcd/python-mimeogram/blob/master/LICENSE.txt
+
+.. image:: https://img.shields.io/pypi/pyversions/python-mimeogram
+   :alt: Python Versions
+   :target: https://pypi.org/project/python-mimeogram/
+
+
+.. todo:: Provide content.
+
+
+`More Flair <https://www.imdb.com/title/tt0151804/characters/nm0431918>`_
+===============================================================================
+
+.. image:: https://img.shields.io/github/last-commit/emcd/python-mimeogram
+   :alt: GitHub last commit
+   :target: https://github.com/emcd/python-mimeogram
+
+.. image:: https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/copier-org/copier/master/img/badge/badge-grayscale-inverted-border-orange.json
+   :alt: Copier
+   :target: https://github.com/copier-org/copier
+
+.. image:: https://img.shields.io/badge/%F0%9F%A5%9A-Hatch-4051b5.svg
+   :alt: Hatch
+   :target: https://github.com/pypa/hatch
+
+.. image:: https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit
+   :alt: pre-commit
+   :target: https://github.com/pre-commit/pre-commit
+
+.. image:: https://img.shields.io/badge/security-bandit-yellow.svg
+   :alt: Bandit
+   :target: https://github.com/PyCQA/bandit
+
+.. image:: https://img.shields.io/badge/linting-pylint-yellowgreen
+   :alt: Pylint
+   :target: https://github.com/pylint-dev/pylint
+
+.. image:: https://microsoft.github.io/pyright/img/pyright_badge.svg
+   :alt: Pyright
+   :target: https://microsoft.github.io/pyright
+
+.. image:: https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/ruff/main/assets/badge/v2.json
+   :alt: Ruff
+   :target: https://github.com/astral-sh/ruff
+
+
+
+.. image:: https://img.shields.io/pypi/implementation/python-mimeogram
+   :alt: PyPI - Implementation
+   :target: https://pypi.org/project/python-mimeogram/
+
+.. image:: https://img.shields.io/pypi/wheel/python-mimeogram
+   :alt: PyPI - Wheel
+   :target: https://pypi.org/project/python-mimeogram/
---
+++ documentation/sphinx/conf.py
@@ -0,0 +1,138 @@
+''' Configuration file for the Sphinx documentation builder.
+
+    This file only contains a selection of the most common options.
+    For a full list, see the documentation:
+        https://www.sphinx-doc.org/en/master/usage/configuration.html
+    Also, see this nice article on Sphinx customization:
+        https://jareddillard.com/blog/common-ways-to-customize-sphinx-themes.html
+'''
+
+# mypy: ignore-errors
+# pylint: disable=consider-using-namedtuple-or-dataclass
+# ruff: noqa: E402,F401
+
+
+def _calculate_copyright_notice( ):
+    from datetime import datetime as DateTime
+    first_year = 2025
+    now_year = DateTime.utcnow( ).year
+    if first_year < now_year: year_range = f"{first_year}-{now_year}"
+    else: year_range = str( first_year )
+    return f"{year_range}, Eric McDonald"
+
+
+def _import_version( ):
+    from importlib import import_module
+    from pathlib import Path
+    from sys import path
+    project_location = Path( __file__ ).parent.parent.parent
+    path.insert( 0, str( project_location / 'sources' ) )
+    module = import_module( 'mimeogram' )
+    return module.__version__
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'python-mimeogram'
+author = 'Eric McDonald'
+copyright = ( # pylint: disable=redefined-builtin
+    _calculate_copyright_notice( ) )
+release = version = _import_version( )
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.graphviz',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.githubpages',
+    'sphinx_copybutton',
+    'sphinx_inline_tabs',
+]
+
+templates_path = [ '_templates' ]
+
+exclude_patterns = [ ]
+
+rst_prolog = f'''
+.. |project| replace:: {project}
+'''
+
+nitpicky = True
+nitpick_ignore = [
+    # Workaround for https://bugs.python.org/issue11975
+    # Found on Stack Overflow (credit to Astropy project):
+    #   https://stackoverflow.com/a/30624034
+    ( 'py:class', "D[k] if k in D, else d.  d defaults to None." ),
+    ( 'py:class', "None.  Remove all items from D." ),
+    ( 'py:class', "a set-like object providing a view on D's items" ),
+    ( 'py:class', "a set-like object providing a view on D's keys" ),
+    ( 'py:class', "an object providing a view on D's values" ),
+    ( 'py:class', "functools.partial" ),
+    ( 'py:class', "mappingproxy" ),
+    ( 'py:class', "module" ),
+    ( 'py:class',
+      "v, remove specified key and return the corresponding value." ),
+    # Type annotation weirdnesses.
+    ( 'py:class', "Doc" ),
+    ( 'py:class', "types.Annotated" ),
+    ( 'py:class', "typing_extensions.Any" ),
+]
+
+# -- Options for linkcheck builder -------------------------------------------
+
+linkcheck_ignore = [
+    # Circular dependency between building HTML and publishing it.
+    r'https://emcd\.github\.io/python-mimeogram/.*',
+    # Stack Overflow rate limits too aggressively, which breaks matrix builds.
+    r'https://stackoverflow\.com/help/.*',
+    # Repository does not exist during initial development.
+    r'https://github\.com/emcd/python-mimeogram',
+    r'https://github\.com/emcd/python-mimeogram/.*',
+    # Package does not exist during initial development.
+    r'https://pypi.org/project/mimeogram/',
+]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# The theme to use for HTML and HTML Help pages.
+# https://github.com/pradyunsg/furo
+html_theme = 'furo'
+html_theme_options = {
+    'navigation_with_keys': True,
+    'sidebar_hide_name': True,
+}
+
+html_static_path = [ '_static' ]
+
+# -- Options for autodoc extension -------------------------------------------
+
+autodoc_default_options = {
+    'member-order': 'groupwise',
+    'imported-members': False,
+    'inherited-members': True,
+    'show-inheritance': True,
+    'undoc-members': True,
+}
+
+#autodoc_typehints = 'description'
+
+# -- Options for intersphinx extension ---------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#configuration
+
+intersphinx_mapping = {
+    'python': (
+        'https://docs.python.org/3', None),
+    'typing-extensions': (
+        'https://typing-extensions.readthedocs.io/en/latest', None),
+}
+
+# -- Options for todo extension ----------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/todo.html#configuration
+
+todo_include_todos = True
